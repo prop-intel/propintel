0cf09977747d7dff650a737150c9810a6ea43d4d
e-roy <eric@fullstackeric.com>
2025-12-13 16:38:59 -0600

    fix eslint errors in db and api backend

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -2,11 +2,12 @@ import { createBaseConfig } from "./base.js";
 
 /**
  * ESLint config for Node.js packages (Lambda, libraries)
+ * @param {string} dirname - import.meta.dirname from the consuming config
  */
-export function createNodeConfig() {
+export function createNodeConfig(dirname) {
   return [
-    { ignores: ['dist', 'node_modules', '.serverless'] },
-    ...createBaseConfig()
+    { ignores: ["dist", "node_modules", ".serverless"] },
+    ...createBaseConfig({ tsconfigRootDir: dirname }),
   ];
 }
 
